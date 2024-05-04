aef79c4d8a1da203a3850c077193e01dccc81cdf
Harf <harf@hexkode.com>
2024-05-04 18:03:19 +0500

    crud operations implemented

--- src/containers/Ledgers/LedgerProducts/lib/api.js
+++ src/containers/Ledgers/LedgerProducts/lib/api.js
@@ -1,15 +1,24 @@
-import { supabase } from "../../../../config/index_supabase";
-
-const { REACT_APP_SUPABASE_TABLE_NAME } = process.env;
+import { supabase, tableName } from "../../../../config/index_supabase";
 
 export const fetchUserLedgerData = async (userId) => {
   return await supabase
-    .from(REACT_APP_SUPABASE_TABLE_NAME)
+    .from(tableName)
     .select()
     .eq("user_id", userId)
     .order("buy_date", { ascending: true });
 };
 
 export const postUserLedgerData = async (formData) => {
-  return await supabase.from(REACT_APP_SUPABASE_TABLE_NAME).insert(formData);
+  return await supabase.from(tableName).insert(formData);
+};
+
+export const deleteUserLedgerData = async (dataID) => {
+  return await supabase.from(tableName).delete().match({ id: dataID });
+};
+
+export const updateUserLedgerData = async (dataID, updateData) => {
+  return await supabase
+    .from(tableName)
+    .update(updateData)
+    .match({ id: dataID });
 };
