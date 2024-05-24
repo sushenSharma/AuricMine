8e5b2579927b431f276ba50e73a1162259c93042
sushenSharma <sushensharma721@gmail.com>
2024-05-23 19:06:45 -0700

    Added Json data in risk management tab

--- src/containers/Main/RiskManagement/index.js
+++ src/containers/Main/RiskManagement/index.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const RiskManagement = () => {
-  return <h1>RiskManagement</h1>;
+  // const ledgerData = useSelector((state) => state.public.ledgerData); Redux appraoch
+  const led = localStorage.getItem("ledgerData");
+
+  return <h1>{JSON.stringify(led, null, 2)}</h1>;
 };
 
 export default RiskManagement;
