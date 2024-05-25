a59dfe9a4829b6f2fc1b1593075c8efd9c9e14ab
sushenSharma <sushensharma721@gmail.com>
2024-05-24 23:35:30 -0700

    Modified Styling and re-ordering of columns

--- src/containers/Main/RiskManagement/index.js
+++ src/containers/Main/RiskManagement/index.js
@@ -1,58 +1,49 @@
 import React, { useState } from "react";
-import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
-import { TextField, Button, Box, Typography } from "@mui/material";
+import { TextField, Button, Box, Typography, Grid } from "@mui/material";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
 
 const RiskManagement = () => {
   const [finalPercentage, setFinalPercentage] = useState(null);
   const [seedCapital, setSeedCapital] = useState("500000"); // Seed capital set to 500,000
   const [initialRisk, setInitialRisk] = useState("5");
+  const [regularIncome, setRegularIncome] = useState(""); // State to store the regular income
+  const [provisioningPercentage, setProvisioningPercentage] = useState(""); // State to store the provisioning percentage
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const saveToDatabase = async () => {
+    console.log("Saving changes...");
+  };
 
   const fetchData = async () => {
     setLoading(true);
     setError(null);
     try {
-      // Fetching the ledgerData from localStorage and parsing it
       const ledgerDataRaw = localStorage.getItem("ledgerData");
       const ledgerData = JSON.parse(JSON.parse(ledgerDataRaw));
-
-      // Extracting profit_loss values into an array
       const profitLossArray = ledgerData.map((item) =>
         parseFloat(item.profit_loss)
       );
-
-      // Define other parameters
       const initialCapital = 1000;
       const initialRiskPercentage = 0.05;
       const profitMultiplier = 1.5;
-
-      // Creating the body of the POST request
       const requestBody = {
         initialCapital,
         initialRiskPercentage,
         tradeOutcomes: profitLossArray,
         profitMultiplier,
       };
-      console.log(requestBody);
-
-      // API call
+      const userID = localStorage.getItem("userId");
       const response = await fetch(
         "https://zcvtgtaimnsrlemslypr.supabase.co/functions/v1/hello-world",
         {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            // Add other headers as needed
-          },
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify(requestBody),
         }
       );
 
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
+      if (!response.ok) throw new Error("Network response was not ok");
       const result = await response.json();
       setFinalPercentage(result.finalRiskPercentage); // Adjust according to your actual API response
     } catch (error) {
@@ -61,65 +52,116 @@ const RiskManagement = () => {
       setLoading(false);
     }
   };
-  const settings = {
-    width: 200,
-    height: 200,
-  };
-  const handleInput = (e, setter) => setter(e.target.value);
 
   return (
     <div>
-      <Box sx={{ maxWidth: 400, margin: "auto", padding: 2 }}>
-        <Typography variant="h4" sx={{ textAlign: "center", marginBottom: 2 }}>
-          Risk Management
-        </Typography>
-        <TextField
-          label="Seed Capital"
-          variant="outlined"
-          fullWidth
-          value={seedCapital}
-          onChange={(e) => handleInput(e, setSeedCapital)}
-          margin="normal"
-        />
-        <TextField
-          label="Initial Risk (%)"
-          variant="outlined"
-          fullWidth
-          value={initialRisk}
-          onChange={(e) => handleInput(e, setInitialRisk)}
-          margin="normal"
-          type="number"
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={fetchData}
-          disabled={loading || !seedCapital || !initialRisk}
-          fullWidth
-          sx={{ marginY: 2 }}
-        >
-          {loading ? "Calculating..." : "Calculate Stop loss for my Next Trade"}
-        </Button>
-        {error && <Typography color="error">Error: {error}</Typography>}
-        {finalPercentage !== null && (
-          <Gauge
-            {...settings}
-            value={finalPercentage}
-            cornerRadius="50%"
-            sx={(theme) => ({
-              [`& .${gaugeClasses.valueText}`]: {
-                fontSize: 24,
-                fill: theme.palette.text.primary,
-              },
-              [`& .${gaugeClasses.valueArc}`]: {
-                fill: "#52b202",
-              },
-              [`& .${gaugeClasses.referenceArc}`]: {
-                fill: theme.palette.grey[300],
-              },
-            })}
-          />
-        )}
+      <Box
+        sx={{
+          maxWidth: "auto",
+          margin: "left",
+          padding: 2,
+          background: "#fff",
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={4}>
+            <Typography
+              variant="h4"
+              sx={{ textAlign: "center", marginBottom: 2 }}
+            >
+              Risk Management Settings
+            </Typography>
+            <TextField
+              label="Seed Capital"
+              variant="outlined"
+              fullWidth
+              value={seedCapital}
+              onChange={(e) => setSeedCapital(e.target.value)}
+              margin="normal"
+            />
+            <TextField
+              label="Initial Risk (%)"
+              variant="outlined"
+              fullWidth
+              value={initialRisk}
+              onChange={(e) => setInitialRisk(e.target.value)}
+              margin="normal"
+              type="number"
+            />
+            <TextField
+              label="Regular Income"
+              variant="outlined"
+              fullWidth
+              value={regularIncome}
+              onChange={(e) => setRegularIncome(e.target.value)}
+              margin="normal"
+              helperText="Enter your regular income from cashflow"
+            />
+            <TextField
+              label="Provisioning Percentage"
+              variant="outlined"
+              fullWidth
+              value={provisioningPercentage}
+              onChange={(e) => setProvisioningPercentage(e.target.value)}
+              margin="normal"
+              type="number"
+              helperText="Percentage of income okay for provisioning loss in trades"
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={saveToDatabase}
+              fullWidth
+              sx={{ marginY: 2 }}
+            >
+              Save Changes
+            </Button>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              paddingY: 10, // Added vertical padding for spacing
+            }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={fetchData}
+              disabled={loading || !seedCapital || !initialRisk}
+              startIcon={<AccountBalanceWalletOutlinedIcon />}
+              sx={{ marginY: 5, borderRadius: 0 }}
+              size="large"
+            >
+              {loading
+                ? "Calculating..."
+                : "Calculate Stop Loss for My Next Trade"}
+            </Button>
+            {error && <Typography color="error">Error: {error}</Typography>}
+            {finalPercentage !== null && (
+              <Typography variant="h5" sx={{ mt: 2, textAlign: "center" }}>
+                Based on your past performance, It is recommended to put a stop
+                loss of{" "}
+                <span
+                  style={{
+                    color: "#d32f2f",
+                    fontStyle: "normal",
+                    fontWeight: "bolder",
+                    fontSize: "30px",
+                  }}
+                >
+                  {finalPercentage}%
+                </span>{" "}
+                for your next trade.
+              </Typography>
+            )}
+          </Grid>
+        </Grid>
       </Box>
     </div>
   );
