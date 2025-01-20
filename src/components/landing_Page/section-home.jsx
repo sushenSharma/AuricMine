979dff535da358a2227e10f193965fda3a5ffcf3
Shushen Sharma <shushen@cyberqp.com>
2025-01-20 01:10:18 -0800

    Modified landing page

--- src/components/landing_Page/section-home.jsx
+++ src/components/landing_Page/section-home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Button,
@@ -6,126 +6,138 @@ import {
   useMediaQuery,
   useTheme,
 } from "@mui/material";
+import Carousel from "react-material-ui-carousel"; // Install this package with npm or yarn
 import "../../assets/styles/landingPage.css"; // Import the CSS file for styling
-import lad from "../../assets/resources/landingpage.png";
+import CarouselImage1 from "../../../src/assets/resources/MainTable.jpg"
+import CarouselImage2 from "../../../src/assets/resources/Carousel2.jpg"
+import CarouselImage3 from "../../../src/assets/resources/carousel3.jpg"
+import CarouselImage4 from "../../../src/assets/resources/Carousel4.jpg"
+import CarouselImage5 from "../../../src/assets/resources/carousel6.jpg"
+
 
 export default function SectionHome({ handleLogin }) {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
+  // Carousel images (replace with actual images)
+  const carouselImages = [
+  CarouselImage1,CarouselImage2,CarouselImage3,CarouselImage4,CarouselImage5
+  ];
+
   return (
-    <Box
-      className="home-section-main"
-      // style={{ backgroundImage: `url(${lad})` }}
-    >
+    <Box className="home-section-main">
       {/* Left container for text */}
       <Box
-  sx={{
-    zIndex: 1,
-    padding: isMobile ? "2rem" : "5rem",
-    width: "100%", // Full width
-  }}
->
-  {/* Set a higher z-index to ensure text appears above the background */}
-  <Typography
-    sx={{
-      fontSize: "3rem", // Adjust font size for smaller screens
-      fontWeight: "600",
-      textTransform: "uppercase",
-      fontFamily: "Jura",
-    }}
-  >
-    SWING TRADING, STREAMLINED <br /> WITH <br /> <span className="gradientText">TRADINGJOURNAL.AI</span>
-  </Typography>
-  <Typography
-    sx={{
-      fontSize: "2rem", // Adjust font size for smaller screens
-      fontWeight: "600",
-      fontFamily: "Jura",
-      paddingY: isMobile ? "2rem" : "5rem",
-      width: isMobile ? null : "75%",
-    }}
-  >
-    Revolutionize your trades with our AI-driven platform. Track, analyze, and improve with ease.
-  </Typography>
-  <ul style={{ listStyleType: "disc", marginLeft: "2rem", fontFamily: "Jura" }}>
-    <li>
-      <Typography
         sx={{
-          fontSize: "1.5rem",
-          fontWeight: "500",
+          zIndex: 1,
+          padding: isMobile ? "2rem" : "5rem",
+          width: "100%",
         }}
       >
-        <strong>Phase-Based Watchlists:</strong> Stay organized by tracking every stock—research, setup, entry, or exit.
-      </Typography>
-    </li>
-    <li>
-      <Typography
-        sx={{
-          fontSize: "1.5rem",
-          fontWeight: "500",
-        }}
-      >
-        <strong>AI-Powered Insights:</strong> Discover what works with tailored recommendations for your swing strategies.
-      </Typography>
-    </li>
-    <li>
-      <Typography
-        sx={{
-          fontSize: "1.5rem",
-          fontWeight: "500",
-        }}
-      >
-        <strong>Proprietary Risk Management Algorithm:</strong> Dynamically adjust stop-losses based on performance for smarter trades.
-      </Typography>
-    </li>
-  </ul>
-  <Typography
-    sx={{
-      fontSize: "3rem", // Adjust font size for smaller screens
-      fontFamily: "Jura",
-      fontWeight: "600",
-    }}
-  >
-  </Typography>
-  <Button
-    type="button"
-    sx={{
-      backgroundColor: "#ffffff",
-      margin: "2rem 0", // Adjust margin for smaller screens
-      fontSize: "16px", // Adjust font size for smaller screens
-      fontFamily: "Jura",
-      color: "#000",
-      padding: "7px 16px 8px",
-      borderRadius: "8px",
-      fontWeight: 700,
-    }}
-    className="signup-button"
-    onClick={handleLogin}
-  >
-    Sign Up
-  </Button>
-</Box>
-
-      {/* Right container for background image */}
-      {
-        <Box
+        <Typography
           sx={{
-            position: "absolute", // Position the background image absolutely
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: isMobile ? 0 : 300,
-            zIndex: 0, // Set a lower z-index so the text appears above
-            backgroundImage: `url(${lad})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            width: "100%",
-            height: isMobile ? "50vh" : "100%", // Adjust height as needed
-            opacity: 0.7,
+            fontSize: "3rem",
+            fontWeight: "600",
+            textTransform: "uppercase",
+            fontFamily: "Jura",
           }}
-        />
-      }
+        >
+          SWING TRADING, STREAMLINED <br /> WITH <br />{" "}
+          <span className="gradientText">TRADINGJOURNAL.AI</span>
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "2rem",
+            fontWeight: "600",
+            fontFamily: "Jura",
+            paddingY: isMobile ? "2rem" : "5rem",
+            width: isMobile ? null : "75%",
+          }}
+        >
+          Revolutionize your trades with our AI-driven platform. Track, analyze,
+          and improve with ease.
+        </Typography>
+        <ul
+          style={{
+            listStyleType: "disc",
+            marginLeft: "2rem",
+            fontFamily: "Jura",
+          }}
+        >
+          <li>
+            <Typography sx={{ fontSize: "1.5rem", fontWeight: "500" }}>
+              <strong>Stage-Based Watchlists :</strong> Map your stock’s journey—research, setup, entry, and exit
+            </Typography>
+          </li>
+          <li>
+            <Typography sx={{ fontSize: "1.5rem", fontWeight: "500" }}>
+              <strong>AI-Powered Insights :</strong> Discover what works with
+              tailored recommendations for your swing strategies.
+            </Typography>
+          </li>
+          <li>
+            <Typography sx={{ fontSize: "1.5rem", fontWeight: "500" }}>
+              <strong>Proprietary Risk Management Algorithm :</strong>{" "}
+              Dynamically adjust stop-losses based on performance for smarter
+              trades.
+            </Typography>
+          </li>
+        </ul>
+        <Button
+          type="button"
+          sx={{
+            backgroundColor: "#ffffff",
+            margin: "2rem 0",
+            fontSize: "16px",
+            fontFamily: "Jura",
+            color: "#000",
+            padding: "7px 16px 8px",
+            borderRadius: "8px",
+            fontWeight: 700,
+          }}
+          className="signup-button"
+          onClick={handleLogin}
+        >
+          Sign Up
+        </Button>
+      </Box>
+
+      {/* Right container for carousel */}
+      <Box
+        sx={{
+          position: "relative",
+          zIndex: 1,
+          padding: "1rem",
+          width: "110%", // Adjust the width as needed
+          height: "200%", // Adjust the height as needed
+          marginRight: isMobile ? "2rem" : "2rem", // Add gap on the right
+          marginLeft: isMobile ? "2rem" : "5rem", // Optionally shift carousel left
+        }}
+      >
+        <Carousel
+          indicators={false}
+          navButtonsAlwaysVisible={true}
+          sx={{
+            height: isMobile ? "300px" : "500px",
+            borderRadius: "10px",
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          {carouselImages.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Feature ${index + 1}`}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                borderRadius: "30px",
+              }}
+            />
+          ))}
+        </Carousel>
+      </Box>
     </Box>
   );
 }
